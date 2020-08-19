b84c3f57ca2c79548ccbb68f5714632bd7e0689d
Max Janik <macksjanik@gmail.com>
2020-08-19 14:34:21 -0400

    Updates

---
+++ precache-manifest.6892f3628489b907e27dac67bdc38d45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0083a61b303673f1361f3b6ff3ce0c37",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "b35176163be838e41d72",
+    "url": "/reactapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "55af577c8a2775914661",
+    "url": "/reactapp/static/css/main.312f9422.chunk.css"
+  },
+  {
+    "revision": "b35176163be838e41d72",
+    "url": "/reactapp/static/js/2.5ec828bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactapp/static/js/2.5ec828bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55af577c8a2775914661",
+    "url": "/reactapp/static/js/main.56634929.chunk.js"
+  },
+  {
+    "revision": "ec5e6060e41010aa547a",
+    "url": "/reactapp/static/js/runtime-main.bac8d581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
